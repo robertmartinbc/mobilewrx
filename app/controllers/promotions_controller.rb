5214b16523672b3975973267d0d4806d3f54f551
Robert Martin <robertmartinbc@shaw.ca>
2013-10-13 12:07:32 -0700

    Added promotions create feature.

--- app/controllers/promotions_controller.rb
+++ app/controllers/promotions_controller.rb
@@ -8,8 +8,32 @@ class PromotionsController < ApplicationController
   end
 
   def new
+    @promotion = Promotion.new
   end
 
+  def create
+  @promotion = Promotion.new(params[:promotion])
+  if @promotion.save
+    flash[:notice] = "Promotion was saved."
+    redirect_to @promotion
+  else
+    flash[:error] = "There was an error saving the promotion. Please try again."
+    render :new
+  end
+end
+
   def edit
+    @promotion = Promotion.find(params[:id])
+  end
+
+  def update
+    @promotion = Promotion.find(params[:id])
+    if @promotion.update_attributes(params[:promotion])
+      flash[:notice] = "Promotion was updated."
+      redirect_to @promotion
+    else
+      flash[:error] = "There was an error saving the promotion. Please try again."
+      render :new
+    end
   end
 end
