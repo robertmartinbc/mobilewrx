1737c0c101589a6541fd0c2de4c796f6baa3318d
Robert Martin <robertmartinbc@shaw.ca>
2013-10-14 14:28:45 -0700

    Changed asset compilation to false.

--- config/application.rb
+++ config/application.rb
@@ -14,11 +14,11 @@ module Mobilewrx
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
-      
+
       g.test_framework :rspec, fixture: true
       g.fixture_replacement :factory_girl, dir: 'spec/factories'
-      
-      
+
+
       g.view_specs false
       g.helper_specs false
     end
@@ -72,5 +72,7 @@ module Mobilewrx
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.initialize_on_precompile = false
   end
 end
