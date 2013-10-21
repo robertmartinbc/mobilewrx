212f3c3d6a24f9878d5e8dede85c4f9be693d7f9
Robert Martin <robertmartinbc@shaw.ca>
2013-10-21 10:50:27 -0700

    Added Google Maps to app.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131018055512) do
+ActiveRecord::Schema.define(:version => 20131021163924) do
 
   create_table "promotions", :force => true do |t|
     t.string   "title"
@@ -61,6 +61,9 @@ ActiveRecord::Schema.define(:version => 20131018055512) do
     t.string   "customer_id"
     t.string   "last_4_digits"
     t.string   "avatar"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.boolean  "gmaps"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
