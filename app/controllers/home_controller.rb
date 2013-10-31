dfe37270436e8abc577b89256092f88496c2b6cc
Robert Martin <robertmartinbc@shaw.ca>
2013-10-31 08:58:45 -0700

    Limited number of random promotions to 4 on front page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 	def index
 		@random_promotions = Promotion.limit(1).order("RANDOM()")
-		@random1_promotions = Promotion.limit(8).order("RANDOM()")
+		@random1_promotions = Promotion.limit(4).order("RANDOM()")
 	end
 end
