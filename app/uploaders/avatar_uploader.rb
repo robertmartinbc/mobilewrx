8ea5f692126fe80fe13d8a0872604e5eee0013eb
Robert Martin <robertmartinbc@shaw.ca>
2013-11-02 09:34:02 -0700

    Added phot to profiled and made some style changes.

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -37,9 +37,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   end
 
   version :small do
-    process :resize_to_fill => [30, 30]
+    process :resize_to_fill => [50, 50]
   end
 
+version :thumb do
+    process :resize_to_fill => [100, 100]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
