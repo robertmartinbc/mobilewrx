efa43cfe4d5e4a5927ff1b298dd6a4d64f3f4bf3
Robert Martin <robertmartinbc@shaw.ca>
2013-11-09 08:57:10 -0800

    Added code for saving credit cards on stripe.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -29,3 +29,35 @@ end
 #
 #      user.purchased_promotions.create!(promotion_id: promotion.id, stripe_charge_id: charge.transaction_id)
 #end
+
+# Set your secret key: remember to change this to your live secret key in production
+# See your keys here https://manage.stripe.com/account
+# Stripe.api_key = "sk_test_J1uwwu0FevHqThdeRVfyk52u"
+
+# # Get the credit card details submitted by the form
+# token = params[:stripeToken]
+
+# # Create a Customer
+# customer = Stripe::Customer.create(
+#   :card => token,
+#   :description => "payinguser@example.com"
+# )
+
+# # Charge the Customer instead of the card
+# Stripe::Charge.create(
+#     :amount => 1000, # in cents
+#     :currency => "cad",
+#     :customer => customer.id
+# )
+
+# # Save the customer ID in your database so you can use it later
+# save_stripe_customer_id(user, customer.id)
+
+# # Later...
+# customer_id = get_stripe_customer_id(user)
+
+# Stripe::Charge.create(
+#   :amount   => 1500, # $15.00 this time
+#   :currency => "cad",
+#   :customer => customer_id
+# )
