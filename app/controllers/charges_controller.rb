c4241f67541b2b859cb316a36a16264d2b50822b
Robert Martin <robertmartinbc@shaw.ca>
2013-11-17 20:34:04 -0800

    Changed params on stripe token.

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -17,9 +17,7 @@ class ChargesController < ApplicationController
                         :description => 'Rails Stripe customer',
                         :currency    => 'usd'
                         )
-                puts "================="
-                p params
-                Purchase.create(promotion_id: @promotion.id, user_id: current_user.id, stripe_reference_id: params[:stripeToken])
+                Purchase.create(promotion_id: @promotion.id, user_id: current_user.id, stripe_reference_id: params[:stripe_token])
         rescue Stripe::CardError => e
                 flash[:error] = e.message
                 redirect_to charges_path
