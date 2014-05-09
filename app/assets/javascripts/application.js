a4b6f54ffc3335a47094272c5aa6e026f00ecfb6
Robert Martin <robertmartinbc@shaw.ca>
2014-05-08 20:30:41 -0700

    Trying to fix dropdown.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,10 +10,10 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //= require jquery
-//= require twitter/bootstrap
 //= require jquery.turbolinks
 //= require jquery_ujs
 //= require bootstrap-datepicker
+//= require twitter/bootstrap
 //= require_tree .
 
 $('.dropdown-toggle').dropdown()
