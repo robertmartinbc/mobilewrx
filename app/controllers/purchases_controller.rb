857aff663081474cfd25ef1781b1a6ba671bb187
Robert Martin <robertmartinbc@shaw.ca>
2014-06-11 08:28:09 -0700

    Added json response to purchases controller.

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,13 @@
 class PurchasesController < ApplicationController
 
+	respond_to :html, :json
+
 	def show
 		@purchase = Purchase.find(params[:id])
     	@promotion = @purchase.promotion
+    	respond_with do |format|
+      format.json { render json: @purchase }
+      format.html { render html: @purchase }
+    end
 	end
 end
